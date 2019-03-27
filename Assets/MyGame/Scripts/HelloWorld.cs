78b81ff3d5db4e0b7ed75d0d307e7e286704e8b9
Kiraay <Habakuk707@gmx.at>
2019-03-27 12:01:54 +0100

    test

--- Assets/MyGame/Scripts/HelloWorld.cs
+++ Assets/MyGame/Scripts/HelloWorld.cs
@@ -1,21 +1,21 @@
 ﻿//smeerws: 
 //printing messages to the unity console
+
 using System;
 using UnityEngine;
+
 public class HelloWorld : MonoBehaviour // Use this for initialization
 {
+
     void Start ()
     {
-        string hw = "Hello World (hw)";
+        string HelloWorldText = "Hello World (hw)";
         print("(print) Hello World");
         Debug.Log("(Debug.Log) Hello World");
         Console.WriteLine("(Console.WriteLine) Hello World"); //Console.WriteLine does nothing inside the Unity Engine,
                                                               //because Unity uses a custom console, only accessed 
-        print("(print - hw) " + hw);
-        Debug.Log("(Debug.Log - hw ) " + hw);                 //through Unity Engine: Debug.Log, ...
-        Console.WriteLine("Console.WriteLine - hw" + hw);     //Unity doesn't display the standard C# Console.
-    }
-    void Update () // Update is called once per frame
-    {
+        print("(print - HelloWorldText) " + HelloWorldText);
+        Debug.Log("(Debug.Log - HelloWorldText ) " + HelloWorldText);                 //through Unity Engine: Debug.Log, ...
+        Console.WriteLine("Console.WriteLine - HelloWorldText" + HelloWorldText);     //Unity doesn't display the standard C# Console.
     }
 }
